77e563d96a2c4e0a31abdffd2c72a2a7b07c8d64
strongsoda <strongsoda2@gmail.com>
2021-05-22 17:22:24 +0530

    display User Profile Image

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,5 +1,6 @@
+/* eslint-disable react-native/no-inline-styles */
 import { Thumbnail } from 'native-base'
-import React from 'react'
+import React, { useState } from 'react'
 import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native'
 import { fontStyles } from '../../constants/fontStyles'
 
@@ -7,96 +8,160 @@ import { fontStyles } from '../../constants/fontStyles'
 
 import Ionicons from 'react-native-vector-icons/Ionicons'
 import { DEFAULT_THEME_COLOR } from '../../constants/colors'
+import { useSelector } from 'react-redux'
 
 const List = ({
-    avatar = false,
-    leftIcon = false,
-    leftIconName = "",
-    leftIconColor = DEFAULT_THEME_COLOR,
-    lefIconSize = 20,
-    rightIcon = true,   
-    avatarText = "",
-    avatarImage = "",
-    title = "",
-    subtitle = "",
-    rightIconName = "chevron-forward",
-    titleStyle = {},
-    subTitleStyle = {},
-    containerStyle = {},
-    onPress = () => { },
-    image = "",
-    defaultKey = false,
-    divider = false
+  avatar = false,
+  leftIcon = false,
+  leftIconName = '',
+  leftIconColor = DEFAULT_THEME_COLOR,
+  lefIconSize = 20,
+  rightIcon = true,
+  avatarText = '',
+  avatarImage = '',
+  title = '',
+  subtitle = '',
+  rightIconName = 'chevron-forward',
+  titleStyle = {},
+  subTitleStyle = {},
+  containerStyle = {},
+  onPress = () => {},
+  image = '',
+  defaultKey = false,
+  divider = false
 }) => {
-    return (
-        <>
-        <TouchableOpacity onPress={onPress} activeOpacity={0.7} style={[styles.mainContainer, containerStyle]}>
-            {avatar && <View>
-                {typeof avatarImage == "string" ? <View style={styles.avatarTitleContainer}>
-                    <Text style={[fontStyles.ProximaBoldH1, styles.avatarText]}>{avatarText}</Text>
-                </View> : <Thumbnail  circular={false} source={avatarImage} />}
-            </View>}
-            {image ? <Image source={image} /> : null}
-            {leftIcon && <View>
-                <Ionicons name={leftIconName} size={lefIconSize} color={leftIconColor} />
-            </View>}
+  const [userImage, setuserImage] = useState(
+    useSelector(state => state?.userReducer.user.payload?.profile_image_url)
+  )
 
-            <View style={styles.centerComponentContainer}>
-                {defaultKey ? 
-                    <View style={{flexDirection: 'row'}}>
-                        <Text style={[fontStyles.ProximaSemiBold, styles.nameText, titleStyle]}>{title}</Text>
-                        <Text style={[fontStyles.ProximaSemiBold, styles.nameText, {left: 10}]}>default</Text>
-                    </View> 
-                :
-                    <Text style={[fontStyles.ProximaSemiBold, styles.nameText, titleStyle]}>{title}</Text>
-                }
-                {subtitle ? <Text style={[fontStyles.ProximaRegularP2, styles.subtitleText, subTitleStyle]}>{subtitle}</Text> : null}
+  return (
+    <>
+      <TouchableOpacity
+        onPress={onPress}
+        activeOpacity={0.7}
+        style={[styles.mainContainer, containerStyle]}>
+        {avatar && (
+          <>
+            {userImage !== 'Not Available' ? (
+              <View
+                style={{
+                  width: 84,
+                  height: 84,
+                  borderRadius: 100,
+                  overflow: 'hidden'
+                }}>
+                <Image
+                  ro
+                  source={{ uri: decodeURIComponent(userImage) }}
+                  style={{ height: 84, width: 84 }}
+                />
+              </View>
+            ) : (
+              <View>
+                {typeof avatarImage == 'string' ? (
+                  <View style={styles.avatarTitleContainer}>
+                    <Text style={[fontStyles.ProximaBoldH1, styles.avatarText]}>
+                      {avatarText}
+                    </Text>
+                  </View>
+                ) : (
+                  <Thumbnail circular={false} source={avatarImage} />
+                )}
+              </View>
+            )}
+          </>
+        )}
+        {leftIcon && (
+          <View>
+            <Ionicons
+              name={leftIconName}
+              size={lefIconSize}
+              color={leftIconColor}
+            />
+          </View>
+        )}
+
+        <View style={styles.centerComponentContainer}>
+          {defaultKey ? (
+            <View style={{ flexDirection: 'row' }}>
+              <Text
+                style={[
+                  fontStyles.ProximaSemiBold,
+                  styles.nameText,
+                  titleStyle
+                ]}>
+                {title}
+              </Text>
+              <Text
+                style={[
+                  fontStyles.ProximaSemiBold,
+                  styles.nameText,
+                  { left: 10 }
+                ]}>
+                default
+              </Text>
             </View>
+          ) : (
+            <Text
+              style={[fontStyles.ProximaSemiBold, styles.nameText, titleStyle]}>
+              {title}
+            </Text>
+          )}
+          {subtitle ? (
+            <Text
+              style={[
+                fontStyles.ProximaRegularP2,
+                styles.subtitleText,
+                subTitleStyle
+              ]}>
+              {subtitle}
+            </Text>
+          ) : null}
+        </View>
 
-
-            {rightIcon && <View style={{ position: "absolute", right: 15 }} >
-                <Ionicons name={rightIconName} size={25} color={"#D3D8DF"} />
-
-            </View>}
-        </TouchableOpacity>
-        {divider && <View style={styles.dividerStyle} />}
-        </>
-    )
+        {rightIcon && (
+          <View style={{ position: 'absolute', right: 15 }}>
+            <Ionicons name={rightIconName} size={25} color={'#D3D8DF'} />
+          </View>
+        )}
+      </TouchableOpacity>
+      {divider && <View style={styles.dividerStyle} />}
+    </>
+  )
 }
 
 export default List
 
-
 const styles = StyleSheet.create({
-    mainContainer: {
-        padding: 10,
-        flexDirection: 'row',
-        alignItems: "center",
-        paddingHorizontal: 10
-    },
-    avatarTitleContainer: {
-        backgroundColor: "#FFBE00",
-        height: 60,
-        width: 60,
-        borderRadius: 100,
-        alignItems: "center",
-        justifyContent: "center"
-    },
-    avatarText: {
-        color: "#fff"
-    },
-    centerComponentContainer: {
-        marginStart: 10
-    },
-    nameText: {
-        color: "#6905DB"
-    },
-    subtitleText: {
-        color: "#6A7C92",
-        marginVertical: 3
-    },
-    dividerStyle: {
-        backgroundColor: '#F1F2FA',
-        height: 1,
-    }
+  mainContainer: {
+    padding: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 10
+  },
+  avatarTitleContainer: {
+    backgroundColor: '#FFBE00',
+    height: 60,
+    width: 60,
+    borderRadius: 100,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  avatarText: {
+    color: '#fff'
+  },
+  centerComponentContainer: {
+    marginStart: 10
+  },
+  nameText: {
+    color: '#6905DB'
+  },
+  subtitleText: {
+    color: '#6A7C92',
+    marginVertical: 3
+  },
+  dividerStyle: {
+    backgroundColor: '#F1F2FA',
+    height: 1
+  }
 })

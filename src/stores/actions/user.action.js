9010884b6929a813f340822f1e291e3b41f94738
strongsoda <strongsoda2@gmail.com>
2021-05-22 17:21:39 +0530

    lint

--- src/stores/actions/user.action.js
+++ src/stores/actions/user.action.js
@@ -1,8 +1,9 @@
+/* eslint-disable no-console */
 import { Alert } from 'react-native'
 import { getUser, postApi, getApi } from '../../api/fakeApiUser'
 import base_url from '../../constants/base_url'
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { ERROR } from '../constants';
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { ERROR } from '../constants'
 export const fetchUserRequest = () => {
   return {
     type: 'FETCH_USER_REQUEST'
@@ -46,129 +47,135 @@ export const fetchDataUser = () => async dispatch => {
   }
 }
 
-export const userRegister = (user) => {
-  return async (dispatch) => {
+export const userRegister = user => {
+  return async dispatch => {
     try {
-      dispatch({ type: "USER_REGISTER_PROCESSING" })
+      dispatch({ type: 'USER_REGISTER_PROCESSING' })
       let { data } = await postApi(`${base_url}/users/create_user`, user)
 
-      console.log("user Registration response", data)
+      console.log('user Registration response', data)
 
       if (data.code == 200) {
-        Alert.alert("Success", "User Registerd Successfully")
+        Alert.alert('Success', 'User Registerd Successfully')
         return Promise.resolve({ status: true })
         // NavigationSer.navigate("SignIn")
       } else {
-        Alert.alert("error", data.message)
+        Alert.alert('error', data.message)
         return Promise.resolve({ status: false })
       }
-
-
     } catch ({ message }) {
-      console.log("registration Error", message)
+      console.log('registration Error', message)
       return Promise.reject({ status: false, message })
     }
   }
 }
 
-export const userLogin = (user) => {
-  return async (dispatch) => {
+export const userLogin = user => {
+  return async dispatch => {
     try {
-      dispatch({ type: "FETCHING" })
+      dispatch({ type: 'FETCHING' })
       let { data } = await postApi(`${base_url}/users/login_user`, user)
 
-
       if (data.code == 200) {
-        dispatch({ type: "FETCH_USER_SUCCESS", payload: data })
-        console.log("user userLogin response", data)
+        dispatch({ type: 'FETCH_USER_SUCCESS', payload: data })
+        console.log('user userLogin response', data)
         return Promise.resolve({ status: true })
         // NavigationSer.navigate("SignIn")
       } else {
-        Alert.alert("error", data.message)
+        Alert.alert('error', data.message)
         dispatch({ type: ERROR })
         return Promise.resolve({ status: false })
       }
-
-
     } catch ({ message }) {
-      console.log("registration Error", message)
-      dispatch({ type: "CLEAR_PROCESSING" })
+      console.log('registration Error', message)
+      dispatch({ type: 'CLEAR_PROCESSING' })
       return Promise.reject({ status: false, message })
     }
   }
 }
 
-export const forgetPass = (obj) => {
-  return async (dispatch) => {
+export const forgetPass = obj => {
+  return async dispatch => {
     try {
-      dispatch({type: 'FETCHING'})
+      dispatch({ type: 'FETCHING' })
       let { data } = await postApi(`${base_url}/users/forgot_password`, obj)
-      const {payload} = data
+      const { payload } = data
       if (data.code == 200) {
         payload.email = obj.email
-        dispatch({ type: "FETCHED_RESET_PASS_TOKEN", payload: payload })
+        dispatch({ type: 'FETCHED_RESET_PASS_TOKEN', payload: payload })
         return Promise.resolve({ status: true })
       } else {
-        Alert.alert("error", data.message)
+        Alert.alert('error', data.message)
         dispatch({ type: ERROR })
         return Promise.resolve({ status: false })
       }
-    } catch ({message}) {
+    } catch ({ message }) {
       dispatch({ type: ERROR })
-      Alert.alert("error", message)
+      Alert.alert('error', message)
       return Promise.reject({ status: false, message })
     }
   }
 }
 
 export const changePass = (obj, authToken) => {
-  return async (dispatch) => {
-    let {data} = await getApi(`${base_url}/users/validate_reset_pw_token/`, authToken , authToken)
-    console.log("isValid validate_reset_pw_token", data,)
-    
-    if(data.code == 200) {
+  return async dispatch => {
+    let { data } = await getApi(
+      `${base_url}/users/validate_reset_pw_token/`,
+      authToken,
+      authToken
+    )
+    console.log('isValid validate_reset_pw_token', data)
+
+    if (data.code == 200) {
       try {
-        dispatch({type: 'FETCHING'})
-        let { data } = await postApi(`${base_url}/users/change_password`, obj, authToken)
-        console.log("data changePass", data)
+        dispatch({ type: 'FETCHING' })
+        let { data } = await postApi(
+          `${base_url}/users/change_password`,
+          obj,
+          authToken
+        )
+        console.log('data changePass', data)
         if (data.code == 200) {
           // Alert.alert("Alert", "Password changed successfully")
           return Promise.resolve({ status: true })
         } else {
-          Alert.alert("Error", data.message)
-          dispatch({ type: ERROR})
+          Alert.alert('Error', data.message)
+          dispatch({ type: ERROR })
           return Promise.resolve({ status: false })
         }
-      } catch ({message}) {
+      } catch ({ message }) {
         dispatch({ type: ERROR })
-        Alert.alert("error", message)
+        Alert.alert('error', message)
         return Promise.reject({ status: false, message })
       }
-    }else {
-      Alert.alert("Error", data.message)
+    } else {
+      Alert.alert('Error', data.message)
       return Promise.reject({ status: false })
     }
-    
   }
 }
 
-export const updateProfile = (obj) => {
-  return async function(dispatch, getState) {
+export const updateProfile = obj => {
+  return async function (dispatch, getState) {
     const { authToken, userId } = getState().userReducer
-    dispatch({type: "FETCHING"})
+    dispatch({ type: 'FETCHING' })
     try {
-      let { data } = await postApi(`${base_url}/users/update_user/${userId}`, obj, authToken)
+      let { data } = await postApi(
+        `${base_url}/users/update_user/${userId}`,
+        obj,
+        authToken
+      )
       if (data.code == 200) {
-        dispatch({ type: "UPDATED_PROFILE_SUCCESS", payload: data })
+        dispatch({ type: 'UPDATED_PROFILE_SUCCESS', payload: data })
         return Promise.resolve({ status: true })
       } else {
         dispatch({ type: ERROR })
-        Alert.alert("error", data.message)
+        Alert.alert('error', data.message)
         return Promise.resolve({ status: false })
       }
-    } catch ({message}) {
+    } catch ({ message }) {
       dispatch({ type: ERROR })
-      Alert.alert("error", message)
+      Alert.alert('error', message)
       return Promise.reject({ status: false, message })
     }
   }
@@ -176,8 +183,8 @@ export const updateProfile = (obj) => {
 
 export const logout = () => {
   return async function (dispatch) {
-      await AsyncStorage.clear()
-      dispatch({type:  "LOGOUT_REQUEST"})
-      return Promise.resolve({status: true})
+    await AsyncStorage.clear()
+    dispatch({ type: 'LOGOUT_REQUEST' })
+    return Promise.resolve({ status: true })
   }
 }
